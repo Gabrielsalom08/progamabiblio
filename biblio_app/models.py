8d4efc7b6d20bc777b0047f3022254c9db4b9cef
Gabrielsalom08 <gabriel.salom@lasallistas.org.mx>
2024-01-29 14:57:02 -0600

    incio crear cambios agregado de tabla de copias dar alta copias autogenreado de clave alumnos libro y copias ademas de creacion de funcion para dar de alta copias y alumnos esta listo editar borrar

--- biblio_app/models.py
+++ biblio_app/models.py
@@ -1,28 +1,43 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
-# Create your models here.
-class alumno (models.Model):
-    def __str__(self) :
-        name= self.nombre+ ' '+ self.apellido+' '+self.grupo
-        return name
-    clave=models.CharField(max_length=20)
-    nombre=models.CharField(max_length=50)
-    apellido=models.CharField(max_length=50)
-    grupo=models.CharField(max_length=10)
 
-class libro(models.Model):
+class Alumno(models.Model):
+    clave = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    grupo = models.IntegerField()
+    sacalibro = models.BooleanField(default=False)
+
     def __str__(self):
-        nombre= self.codigolibro+' '+self.titulo 
-        return nombre
-    codigolibro = models.CharField(max_length=45, primary_key=True)
+        return f"{self.nombre} {self.apellido} "
+
+
+class Libro(models.Model):
+    codigolibro = models.AutoField(primary_key=True)  # Autogenerar un entero para el código del libro
     titulo = models.CharField(max_length=128)
     autor = models.CharField(max_length=100)
     ilustrador = models.CharField(max_length=100, null=True, blank=True)
-    fechapublicacion = models.DateField(null=True, blank=True)
+    fechapublicacion = models.IntegerField(null=True, blank=True)  # Modificación a IntegerField
     editorial = models.CharField(max_length=100)
     numerotomo = models.CharField(max_length=100, null=True, blank=True)
     caracteristicasespeciales = models.TextField(null=True, blank=True)
-    disponible = models.BooleanField(default=True)
-    ubicacionbiblio = models.CharField(max_length=45, null=True, blank=True)
+    dewy = models.CharField(max_length=45, null=True, blank=True)  # Cambio de nombre a 'dewy'
     publicodirigido = models.CharField(max_length=45, null=True, blank=True)
-    
+
+    def __str__(self):
+        return f"{self.codigolibro} - {self.titulo}"  # Cambio en la representación como cadena
+
+class Copia(models.Model):
+    clavecopia = models.AutoField(primary_key=True)  # Autogenerar un entero para la clave de copia
+    codigolibro = models.ForeignKey(Libro, on_delete=models.CASCADE)  # Llave foránea de Libro
+    disponible = models.BooleanField(default=True)
+    clavealumno = models.ForeignKey('Alumno', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"Copia {self.clavecopia} de {self.codigolibro.titulo}"
+
+@receiver(pre_delete, sender=Libro)
+def eliminar_copias_asociadas(sender, instance, **kwargs):
+    Copia.objects.filter(codigolibro=instance).delete()
--- biblio_app/views.py
+++ biblio_app/views.py
@@ -44,105 +44,98 @@ def login_view(request):
 def alumno_pest(request):
     query = request.GET.get('q')
     if query:
-        # Realiza la búsqueda utilizando el campo 'nombre' o 'apellido' o clave o grupo
-        alumnos = alumno.objects.filter(Q(nombre__icontains=query) | Q(apellido__icontains=query)|Q(clave__icontains=query) |Q(grupo__icontains=query) )
+        # Realiza la búsqueda utilizando el campo 'nombre', 'apellido', 'clave' o 'grupo'
+        alumnos = Alumno.objects.filter(
+            Q(nombre__icontains=query) |
+            Q(apellido__icontains=query) |
+            Q(nombre__icontains=query.split()[0]) &  # Busca el primer nombre
+            Q(apellido__icontains=query.split()[-1]) |# Busca el último nombre
+            Q(clave__icontains=query) |
+            Q(grupo__icontains=query)
+        ).distinct()
     else:
         # Si no hay búsqueda, muestra todos los alumnos
-        alumnos = alumno.objects.all()
+        alumnos = Alumno.objects.all()
 
     return render(request, "alumnos.html", {"current_tab": "alumno", "alumnos": alumnos})
 
 
 def agregar_alum(request):
-    alumno_item= alumno(clave=request.POST['clave'],
-                        nombre=request.POST['nombre'],
-                        apellido=request.POST['apellido'],
-                        grupo=request.POST['grupo'],)
-    alumno_item.save()
-    return redirect('/alumno')
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        grupo = request.POST.get('grupo')
+
+        # Verificar si los campos obligatorios no están vacíos
+        if not nombre or not apellido or not grupo:
+            return render(request, 'error.html', {'mensaje': 'Por favor, completa todos los campos obligatorios.'})
+
+        try:
+            # Intentar crear una instancia de Alumno con los datos proporcionados
+            alumno_item = Alumno(nombre=nombre, apellido=apellido, grupo=grupo)
+            alumno_item.full_clean()  # Validar los datos del modelo
+            alumno_item.save()  # Guardar el objeto en la base de datos
+            return redirect('/alumno')
+        except ValidationError as e:
+            # Si se produce una excepción de validación, mostrar un mensaje de error
+            return render(request, 'error.html', {'mensaje': '; '.join(e.messages)})
+
+    return render(request, 'formulario_alumno.html')
 
 def alumno_detalle(request, pk):
-    # Usamos get_object_or_404 para obtener el objeto alumno o retornar un error 404 si no se encuentra
-    alumno_obj = get_object_or_404(alumno, pk=pk)
-    
+    alumno_obj = get_object_or_404(Alumno, clave=pk)
     return render(request, 'alumno_detalle.html', {'alumno': alumno_obj})
 
-
-
-# ... tus funciones existentes ...
-
 def editar_alumno(request, pk):
-    # Usamos get_object_or_404 para obtener el objeto alumno o retornar un error 404 si no se encuentra
-    alumno_obj = get_object_or_404(alumno, pk=pk)
+    alumno_obj = get_object_or_404(Alumno, clave=pk)
 
     if request.method == 'POST':
-        # Procesa el formulario de edición aquí
         alumno_obj.nombre = request.POST['nombre']
         alumno_obj.apellido = request.POST['apellido']
-        alumno_obj.clave = request.POST['clave']
         alumno_obj.grupo = request.POST['grupo']
         alumno_obj.save()
-
-        # Después de guardar los cambios, redirige a la página de alumnos
         return redirect('/alumno')
 
     return render(request, 'editar_alumno.html', {'alumno': alumno_obj})
 
-
-
 def eliminar_alumno(request, pk):
-    try:
-        alumno_obj = get_object_or_404(alumno, pk=pk)
+    alumno_obj = get_object_or_404(Alumno, clave=pk)
+    print(alumno_obj)
 
-        if request.method == 'POST':
-            # Utiliza el método delete() en el queryset, no en la instancia del modelo
-            alumno.objects.filter(pk=pk).delete()
-
-            if request.is_ajax():
-                return redirect('/alumno')
-
-            # Después de eliminar, redirige a la página de alumnos
-            return redirect('/alumno')
-
-    except Exception as e:
-        # Devuelve detalles sobre el error en la respuesta AJAX
-        return JsonResponse({'error': str(e)}, status=500)
+    if request.method == 'POST':
+        alumno_obj.delete()
+        return redirect('/alumno')
 
     return render(request, 'borrar_alumno.html', {'alumno': alumno_obj})
 
 def cargar_desde_excel(request):
-    if request.method == 'POST':
-        if 'excel_file' in request.FILES:
-            excel_file = request.FILES['excel_file']
-            df = pd.read_excel(excel_file)
+    if request.method == 'POST' and 'excel_file' in request.FILES:
+        excel_file = request.FILES['excel_file']
+        df = pd.read_excel(excel_file)
 
-            # Verificar si todas las columnas necesarias están presentes en el DataFrame
-            required_columns = ['clave', 'nombre', 'apellido', 'grupo']
-            missing_columns = [col for col in required_columns if col not in df.columns]
+        required_columns = ['nombre', 'apellido', 'grupo']
+        missing_columns = [col for col in required_columns if col not in df.columns]
 
-            if missing_columns:
-                missing_columns_str = ', '.join(missing_columns)
-                return HttpResponse(f"El archivo Excel no tiene las columnas necesarias: {missing_columns_str}.")
+        if missing_columns:
+            missing_columns_str = ', '.join(missing_columns)
+            return HttpResponse(f"El archivo Excel no tiene las columnas necesarias: {missing_columns_str}.")
 
-            for index, row in df.iterrows():
-                alumno_item = alumno(
-                    clave=row['clave'],
-                    nombre=row['nombre'],
-                    apellido=row['apellido'],
-                    grupo=row['grupo'],
-                )
-                alumno_item.save()
+        for index, row in df.iterrows():
+            alumno_item = Alumno(
+                nombre=row['nombre'],
+                apellido=row['apellido'],
+                grupo=row['grupo'],
+            )
+            alumno_item.save()
 
-            return redirect('/alumno')
+        return redirect('/alumno')
 
     return render(request, 'tu_template_excel.html')
 
 def borrar_todos_los_alumnos(request):
     if request.method == 'GET':
-        # Realiza la lógica para borrar todos los registros de la tabla de alumnos
-        alumno.objects.all().delete()
-
-        # Redirige de vuelta a la página de alumnos
+        Alumno.objects.all().delete()
         return redirect('/alumno')
 
     return HttpResponse("Solicitud no válida.")
@@ -150,91 +143,92 @@ def borrar_todos_los_alumnos(request):
 def libros_pest(request):
     query = request.GET.get('q')
     if query:
-        # Realiza la búsqueda utilizando el campo 'nombre' o 'apellido' o clave o grupo
-        libros = libro.objects.filter(Q(codigolibro__icontains=query) | Q(titulo__icontains=query)|Q(autor__icontains=query) |Q(editorial__icontains=query) )
+        libros = Libro.objects.filter(Q(codigolibro__icontains=query) | Q(titulo__icontains=query) | Q(autor__icontains=query) | Q(editorial__icontains=query))
     else:
-        # Si no hay búsqueda, muestra todos los alumnos
-        libros = libro.objects.all()
+        libros = Libro.objects.all()
 
-    return render(request,"libros.html",context={"current_tab": "libro", "libros": libros})
+    return render(request, "libros.html", context={"current_tab": "libro", "libros": libros})
 
 def agregar_libros(request):
-    fechapubl_str = request.POST.get('fechapubl', '')
-    if fechapubl_str:
-        try:
-            fechapubl = datetime.strptime(fechapubl_str, '%Y-%m-%d').date()
-        except ValueError:
-            # Handle invalid date format
-            raise ValidationError('Invalid date format. Please use YYYY-MM-DD.')
+    if request.method == 'POST':
+        fechapubl_str = request.POST.get('fechapubl', '')
+        if fechapubl_str:
+            try:
+                fechapubl = int(fechapubl_str)
+            except ValueError:
+                raise ValidationError('Invalid integer format for fechapublicacion. Please enter a valid integer.')
+        else:
+            fechapubl = None
+
+        libro_item = Libro(
+            titulo=request.POST['titulo'],
+            autor=request.POST['autor'],
+            editorial=request.POST['editorial'],
+            numerotomo=request.POST['numtomo'],
+            dewy=request.POST['ubicacion'],  # Ahora 'dewy' en lugar de 'ubicacion'
+            publicodirigido=request.POST['publico'],
+            fechapublicacion=fechapubl,
+            caracteristicasespeciales=request.POST['caracteristicas'],
+        )
+        libro_item.save()
+        return redirect('/libro')
+
+def agregar_copia(request):
+    if request.method == 'POST':
+        # Obtener el código del libro del formulario
+        codigolibro = request.POST.get('codigolibro')
+        
+        # Verificar si el libro con ese código existe en la base de datos
+        libro = get_object_or_404(Libro, codigolibro=codigolibro)
+        
+        # Si el libro existe, crear una instancia de Copia utilizando el libro obtenido
+        if libro:
+            copia_item = Copia(codigolibro=libro)
+            
+            # Guardar la instancia de Copia en la base de datos
+            copia_item.save()
+            
+            # Redireccionar a la página de libros
+            return redirect('/libro')
+        else:
+            # Si el libro no existe, puedes mostrar un mensaje de error o redireccionar a una página de error
+           return redirect('/libro')
     else:
-        # Handle case where date is empty
-        fechapubl = None
-    libro_item= libro(codigolibro=request.POST['clave'],
-                        titulo=request.POST['titulo'],
-                        autor=request.POST['autor'],
-                        ilustrador=request.POST['ilustrador'],
-                        editorial=request.POST['editorial'],
-                        numerotomo=request.POST['numtomo'],
-                        ubicacionbiblio=request.POST['ubicacion'],
-                        publicodirigido=request.POST['publico'],
-                        fechapublicacion=fechapubl,
-                        caracteristicasespeciales=request.POST['caracteristicas'],
-                        disponible=True)
-    libro_item.save()
-    return redirect('/libro')
+        # Si la solicitud no es POST, probablemente deberías manejarla de alguna manera
+        # Por ejemplo, podrías mostrar un mensaje de error o redirigir a una página de error
+        return redirect('/')
+
 
 def libro_detalle(request, codigolibro):
-    # Usamos get_object_or_404 para obtener el objeto alumno o retornar un error 404 si no se encuentra
-    libro_obj = get_object_or_404(libro, codigolibro=codigolibro)
-    
+    libro_obj = get_object_or_404(Libro, codigolibro=codigolibro)
     return render(request, 'libros_detalle.html', {'libro': libro_obj})
 
-
-
 def editar_libro(request, codigolibro):
-    # Usamos get_object_or_404 para obtener el objeto libro o retornar un error 404 si no se encuentra
-    libro_obj = get_object_or_404(libro, codigolibro=codigolibro)
+    libro_obj = get_object_or_404(Libro, codigolibro=codigolibro)
 
     if request.method == 'POST':
         # Procesa el formulario de edición aquí
         libro_obj.titulo = request.POST['titulo']
         libro_obj.autor = request.POST['autor']
         libro_obj.ilustrador = request.POST['ilustrador']
-
-        # Convertir la fecha al formato esperado
-        try:
-            fechapublicacion = datetime.strptime(request.POST['fechapublicacion'], '%b. %d, %Y').strftime('%Y-%m-%d')
-            libro_obj.fechapublicacion = fechapublicacion
-        except ValueError:
-            return HttpResponse("Invalid date format. Please enter the date in the format 'Mon. Day, Year'.")
-
         libro_obj.editorial = request.POST['editorial']
         libro_obj.numerotomo = request.POST['numerotomo']
         libro_obj.caracteristicasespeciales = request.POST['caracteristicasespeciales']
-
-        # Convertir 'on' to True, otherwise set to False
-        libro_obj.disponible = request.POST.get('disponible') == 'on'
-
-        libro_obj.ubicacionbiblio = request.POST['ubicacionbiblio']
+        libro_obj.dewy = request.POST['ubicacionbiblio']
         libro_obj.publicodirigido = request.POST['publicodirigido']
+        try:
+            fechapublicacion = int(request.POST['fechapublicacion'])
+            libro_obj.fechapublicacion = fechapublicacion
+        except ValueError:
+            return HttpResponse("Invalid integer format for fechapublicacion. Please enter a valid integer.")
         libro_obj.save()
-
-        # Después de guardar los cambios, redirige a la página de libros
         return redirect('/libro')
-
-    return render(request, 'editar_libro.html', {'libro': libro_obj})
-
 
 def eliminar_libro(request, codigolibro):
-    libro_obj = get_object_or_404(libro, codigolibro=codigolibro)
-
+    libro_obj = get_object_or_404(Libro, codigolibro=codigolibro)
     if request.method == 'POST':
-        # Utiliza el método delete() en el queryset, no en la instancia del modelo
-        libro.objects.filter(codigolibro=codigolibro).delete()
-
-        # Después de eliminar, redirige a la página de libros
+        libro_obj.delete()
         return redirect('/libro')
-
     return render(request, 'eliminar_libro.html', {'libro': libro_obj})
 
 def cargar_desde_excel_libro(request):
@@ -242,24 +236,16 @@ def cargar_desde_excel_libro(request):
         if 'excel_file' in request.FILES:
             excel_file = request.FILES['excel_file']
             df = pd.read_excel(excel_file)
-
-            # Verificar si todas las columnas obligatorias están presentes en el DataFrame
             required_columns = ['codigolibro', 'titulo', 'autor', 'editorial']
             missing_columns = [col for col in required_columns if col not in df.columns]
-
             if missing_columns:
                 missing_columns_str = ', '.join(missing_columns)
                 return HttpResponse(f"El archivo Excel no tiene las columnas obligatorias: {missing_columns_str}.")
-
             for index, row in df.iterrows():
-                # Verificar campos obligatorios
                 if any(pd.isna(row[col]) for col in required_columns):
                     return HttpResponse("Los campos obligatorios no pueden estar vacíos.")
-
-                # Establecer valor por defecto para fechapublicacion si está ausente
                 fechapublicacion = row.get('fechapublicacion', datetime.now().date())
-
-                libro_item = libro(
+                libro_item = Libro(
                     codigolibro=row['codigolibro'],
                     titulo=row['titulo'],
                     autor=row['autor'],
@@ -268,48 +254,37 @@ def cargar_desde_excel_libro(request):
                     fechapublicacion=fechapublicacion,
                     numerotomo=row.get('tomo', ''),
                     caracteristicasespeciales=row.get('caracteristicas', ''),
-                    disponible=True,
-                    ubicacionbiblio=row.get('ubicacionbiblio', ''),
+                    dewy=row.get('ubicacionbiblio', ''),  # Ajustar según sea necesario
                     publicodirigido=row.get('publico', ''),
-                    # Agrega otros campos según sea necesario
                 )
                 libro_item.save()
-
             return redirect('/libro')
-
     return render(request, 'tu_template_excel.html')
-
 
 def borrar_todos_los_libros(request):
     if request.method == 'GET':
-        # Realiza la lógica para borrar todos los registros de la tabla de alumnos
-        libro.objects.all().delete()
-
-        # Redirige de vuelta a la página de alumnos
+        Libro.objects.all().delete()
         return redirect('/libro')
-
     return HttpResponse("Solicitud no válida.")
+
 def eliminar_libros_por_titulo(request):
     if request.method == 'POST':
         titulo = request.POST.get('titulo', None)
-
         if titulo:
-            # Utiliza el método delete() en el queryset para eliminar todos los libros con el mismo título
-            libro.objects.filter(titulo=titulo).delete()
-
+            Libro.objects.filter(titulo=titulo).delete()
     return redirect('/libro')
+    
 def busqueda_pest(request):
     query = request.GET.get('q')
     if query:
-        # Realiza la búsqueda utilizando el campo 'nombre' o 'apellido' o clave o grupo
-        libros = libro.objects.filter(Q(codigolibro__icontains=query) | Q(titulo__icontains=query)|Q(autor__icontains=query) |Q(editorial__icontains=query)|Q(ilustrador__icontains=query) )
+        libros = Libro.objects.filter(Q(codigolibro__icontains=query) | Q(titulo__icontains=query)|Q(autor__icontains=query) |Q(editorial__icontains=query)|Q(ilustrador__icontains=query) )
     else:
-        # Si no hay búsqueda, muestra todos los alumnos
-        libros = libro.objects.all()
+        # Si no hay búsqueda, muestra todos los libros
+        libros = Libro.objects.all()
 
     return render(request,"busqueda.html",context={"current_tab": "busqueda", "libros": libros})
 def busqeda_detalle(request, codigolibro):
     # Usamos get_object_or_404 para obtener el objeto alumno o retornar un error 404 si no se encuentra
-    libro_obj = get_object_or_404(libro, codigolibro=codigolibro)
+    libro_obj = get_object_or_404(Libro, codigolibro=codigolibro)
     
     return render(request, 'busquedadetalle.html', {'libro': libro_obj})
