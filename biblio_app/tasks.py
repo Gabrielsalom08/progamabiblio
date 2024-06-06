15d3c03174b5893a0adfda9d7d4de6ec8625de79
Gabrielsalom08 <gabriel.salom@lasallistas.org.mx>
2024-06-06 12:51:57 -0600

    cambios para entrega final

--- biblio_app/tasks.py
+++ biblio_app/tasks.py
@@ -26,12 +26,11 @@ class TaskSingleton:
         # Task to update multas
         multas_trigger = CronTrigger(day_of_week='mon-fri', hour=8, minute='30-59')
         self.scheduler.add_job(self.actualizar_multas, trigger=multas_trigger, misfire_grace_time=3600, max_instances=1)
-        print("Scheduler added job for actualizar_multas")  # Debugging
+        
 
         # Task to update the database every hour
         update_trigger = CronTrigger(minute='0-5')  # Runs every hour
         self.scheduler.add_job(self.update_last_update, trigger=update_trigger, misfire_grace_time=3600, max_instances=1)
-        print("Scheduler added job for update_last_update")  # Debugging
 
         self.scheduler.start()
         print("Scheduler started")  # Debugging
@@ -45,12 +44,9 @@ class TaskSingleton:
 
     def actualizar_multas(self):
         self.ensure_db_connection()
-        print("Executing actualizar_multas")  # Debugging
         from .models import Prestamo, Multa
         prestamos_activos = Prestamo.objects.filter(activo=True, regreso__lt=timezone.now().date())
-        print("Total active loans:", prestamos_activos.count())  # Debugging
         for prestamo in prestamos_activos:
-            print("Processing loan:", prestamo.id)  # Debugging
             multas = Multa.objects.filter(alumno=prestamo.clave_alumno)
             if multas.exists():
                 for multa in multas:
@@ -63,12 +59,11 @@ class TaskSingleton:
 
     def update_last_update(self):
         self.ensure_db_connection()
-        print("Executing update_last_update")  # Debugging
         from .models import LastUpdate
         last_update = LastUpdate.get_solo()
         last_update.timestamp = timezone.now()
         last_update.save()
-        print("Updated LastUpdate timestamp")  # Debugging
+        
 
     def ensure_db_connection(self):
         if connection.connection and not connection.is_usable():
